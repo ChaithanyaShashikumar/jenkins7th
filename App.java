87df4c3561ab9a481656306855babd6b86d73187
ChaithanyaShashikumar <chaithanyadheeraj06@gmail.com>
2025-12-23 21:37:35 +0530

    Intial commit

---
+++ App.java
@@ -0,0 +1,8 @@
+package com.example ;
+public class App{
+    public static void main(String[] args){
+        System.out.println("maven build project executed successfully");
+        System.out.println("Jenkins Poll SCM automatation working");
+
+    }
+}
